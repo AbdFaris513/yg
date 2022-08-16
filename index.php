e96b8be1f82a411bdb2d5f71b39ee8d6f0b45f35
AbdFaris513 <106486197+AbdFaris513@users.noreply.github.com>
2022-08-16 13:00:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Harley Store Room</title>
+</head>
+<body>
+    <div class="landing">
+        <header>
+            <a href="#" id="logo">Harley Devidson</a>
+            <nav>
+                <a href="#start">Models</a>
+                <a href="#location">Locastion</a>
+                <a href="">Contact</a>
+            </nav>
+        </header>
+        <div class="maintext">
+            <div class="tag1">World Styelish Motor Bike's</div>
+            <div class="tag2">Handmade Motors</div>
+            <div class="contact">CAll @+91 9876543210</div>
+                <button>Perches</button>
+        </div>
+    </div>
+    <h2 class="headermenu">Menu</h2>
+    <div class="menu">
+        <div id="start" class="starters">
+            <table>
+                <th colspan="2">Starters</th>
+                <tr>
+                    <td>Iron 833</td>
+                    <td>&#8377 15L</td>
+                </tr>
+                <tr>
+                    <td>Fat Boy</td>
+                    <td>&#8377 18L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+            </table>
+        </div>
+        <div class="Models">
+            <table>
+                <th colspan="2">Models</th>
+                <tr>
+                    <td>Iron 833</td>
+                    <td>&#8377 15L</td>
+                </tr>
+                <tr>
+                    <td>Fat Boy</td>
+                    <td>&#8377 18L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Iron 833</td>
+                    <td>&#8377 15L</td>
+                </tr>
+                <tr>
+                    <td>Fat Boy</td>
+                    <td>&#8377 18L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+            </table>
+        </div>
+        <div class="starters">
+            <table>
+                <th colspan="2">Prodects
+
+                </th>
+                <tr>
+                    <td>Iron 833</td>
+                    <td>&#8377 15L</td>
+                </tr>
+                <tr>
+                    <td>Fat Boy</td>
+                    <td>&#8377 18L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Iron 833</td>
+                    <td>&#8377 15L</td>
+                </tr>
+                <tr>
+                    <td>Fat Boy</td>
+                    <td>&#8377 18L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+                <tr>
+                    <td>Forty Eight</td>
+                    <td>&#8377 50L</td>
+                </tr>
+                <tr>
+                    <td>Road King</td>
+                    <td>&#8377 25L</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <h1 class="headeloc">Locastion</h1>
+    <div id="location" class="location">
+        <div class="india">
+            <h3>Chennai</h3>
+            <address>
+                15, Main Road <br>
+                Anna Nager, <br>
+                Chennai - 600102
+            </address>
+        </div>
+        <div class="india">
+            <h3>Mumbai</h3>
+            <address>
+                1/3, Dhalal Street <br>
+                Rocky Nager, <br>
+                Mumbai - 600166
+            </address>
+        </div>
+        <div class="india">
+            <h3>Bengalore</h3>
+            <address>
+                9, Nethagi Road <br>
+                Crent Nager, <br>
+                Bengalore - 600002
+            </address>
+        </div>
+        <div class="india">
+            <h3>Delhi</h3>
+            <address>
+                5, Ganthi Road <br>
+                Pattani Nager, <br>
+                Delhi - 601102
+            </address>
+        </div>
+    </div>
+</body>
+</html>
